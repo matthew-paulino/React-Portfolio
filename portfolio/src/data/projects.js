34c312c29aa0737cb009ebc8bf2caad123ae43ae
Matthew Paulino <matthewpaulino@Matthews-MacBook-Pro.local>
2019-09-10 09:22:37 -0700

    First commit

---
+++ portfolio/src/data/projects.js
@@ -0,0 +1,29 @@
+import project1 from '../assets/project1.png';
+import project2 from '../assets/project2.png';
+import project3 from '../assets/project3.png';
+
+const PROJECTS = [
+  {
+    id: 1,
+    title: 'Example React Application',
+    description: 'A React app that I built, involving JS and core web dev concepts!',
+    link: 'https://github.com/matthew-paulino/portfolio',
+    image: project1
+  },
+  {
+    id: 2,
+    title: 'MY API',
+    description: 'A REST API that I built from scratch with GET and POST requests!',
+    link: 'https://github.com/matthew-paulino/app',
+    image: project2
+  },
+  {
+    id: 3,
+    title: 'UCI Computer Science Project',
+    description: 'My unique project for my Computer Science course.',
+    link: 'https://github.com/matthew-paulino/GitCode',
+    image: project3
+  }
+];
+
+export default PROJECTS;
---
+++ portfolio/src/data/socialProfiles.js
@@ -0,0 +1,30 @@
+import emailIcon from '../assets/email_icon.png';
+import githubIcon from '../assets/github_icon.png';
+import linkedinIcon from '../assets/linkedin_icon.png';
+import twitterIcon from '../assets/twitter_icon.png';
+
+
+const SOCIAL_PROFILES = [
+  {
+    id: 1,
+    link: 'mailto:matthewpaulino@gmail.com',
+    image: emailIcon
+  },
+  {
+    id: 2,
+    link: 'https://github.com/matthew-paulino',
+    image: githubIcon
+  },
+  {
+    id: 3,
+    link: 'https://www.linkedin.com/in/matthew-paulino/',
+    image: linkedinIcon
+  },
+  {
+  id: 4,
+  link: 'https://twitter.com/matthew-paulino',
+  image: twitterIcon
+  }
+];
+
+export default SOCIAL_PROFILES;
